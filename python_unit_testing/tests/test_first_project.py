e776b6ac8f84868ee42faf1e77a291ff8cec18d4
Antonio Martin <a.martin.alcantara@gmail.com>
2021-07-19 17:23:40 +0200

    Added medium test to first_project.py

---
+++ python_unit_testing/tests/test_first_project.py
@@ -0,0 +1,26 @@
+import unittest
+from first_project import avg
+
+class EasyTestCase(unittest.TestCase):
+
+    def test_easy_input(self):
+        self.assertEqual(avg(1, 2, 3), 2)
+
+    def test_easy_input_two(self):
+        self.assertEqual(avg(10, 10, 10, 10, 10), 10)
+
+class MediumTestCase(unittest.TestCase):
+
+    def test_medium_input(self):
+        # If this line of code is throwing a TypeError, then this means that
+        # this test passed, and the function is doing what it is supposed to do
+        with self.assertRaises(TypeError):
+            self.assertEqual(avg(1, 2, 3, "Antonio"), 2)
+
+    def test_medium_input_two(self):
+        with self.assertRaises(TypeError):
+            self.assertEqual(avg(10, 10, 10, 10, "10"), 10)
+
+# Is that this file being run directly by Python or is it being imported?
+if __name__ == '__main__':
+    unittest.main()
