72fa8e90dc0bee30be5d2668d02574e1bec5f948
Antonio Martin <a.martin.alcantara@gmail.com>
2021-07-22 17:11:26 +0200

    Done 2nd-challenge easy tests

--- second_challenge_skeleton/tests/test_second_challenge.py
+++ second_challenge_skeleton/tests/test_second_challenge.py
@@ -7,24 +7,34 @@ class EasyTestCase(unittest.TestCase):
     def setUp(self):
         # Todo: create an object named car from the Car class
         # Todo: use the object car to start the car.
-        pass
+        self.car = Car()
+        self.car.start_car()
 
     def test_easy_input(self):
         # Todo: use the object car to add speed 4 times.
         # Todo: make sure that the current speed is 20.
-        pass
+        self.car.add_speed()
+        self.car.add_speed()
+        self.car.add_speed()
+        self.car.add_speed()
+        self.assertEqual(self.car.current_speed(), 20)
 
     def test_easy_input_two(self):
         # Todo: use the object car to add speed 2 times.
         # Todo: use the object car to stop the car.
         # Todo: make sure that the current speed is 0 not -10.
-        pass
+        self.car.add_speed()
+        self.car.add_speed()
+        self.car.stop()
+        self.assertEqual(self.car.current_speed(), 0)
 
     def tearDown(self):
         # Todo: stop the car.
         # Todo: turn off the car.
         # Todo: set the object car to None.
-        pass
+        self.car.stop()
+        self.car.turn_off_car()
+        self.car = None
 
 
 class MediumTestCase(unittest.TestCase):
